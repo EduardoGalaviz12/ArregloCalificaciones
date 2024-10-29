20857255cd024e442569904c579a6707fa09485c
EduardoGalaviz12 <eduardo.galaviz.aleman@cbtis89.edu.mx>
2024-10-29 08:33:01 -0600

    Add files via upload

---
+++ ArrayCalificaciones.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title></title>
+</head>
+<body>
+<?php 
+$lucia=rand(0,10);
+$ana=rand(0,10);
+$fernando=rand(0,10);
+$David=rand(0,10);
+$oliver=rand(0,10);
+echo "la calificacion de lucia es: " . $lucia;
+echo "<br>";
+echo "la calificacion de ana es: " . $ana;
+echo "<br>";
+echo "la calificacion de fernando es: " . $fernando;
+echo "<br>";
+echo "la calificacion de David es: " . $David;
+echo "<br>";
+echo "la calificacion de oliver es: " . $oliver
+?>
+</body>
+</html>
